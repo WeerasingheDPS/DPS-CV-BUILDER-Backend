7de268a922ce37a94fe8fcd1a4c753cbaeeabf64
WeerasingheDPS <dulanjana.weerasinghe@circles.asia>
2024-01-17 23:09:22 +0530

    Create package structure

---
+++ DPS-CV_BUILDER/src/test/java/dpscvbuilder/com/DPSCV_BUILDER/DpsCvBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package dpscvbuilder.com.DPSCV_BUILDER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpsCvBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
