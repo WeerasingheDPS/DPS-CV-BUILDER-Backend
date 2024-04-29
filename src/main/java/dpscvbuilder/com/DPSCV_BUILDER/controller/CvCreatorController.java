7ac7fdc31f214f30728547a5e78f2813e2ab92a1
WeerasingheDPS <dulanjana.weerasinghe@circles.asia>
2024-04-29 12:26:41 +0530

    change docker file
    Signed-off-by: WeerasingheDPS <dulanjana.weerasinghe@circles.asia>

--- src/main/java/dpscvbuilder/com/DPSCV_BUILDER/controller/CvCreatorController.java
+++ src/main/java/dpscvbuilder/com/DPSCV_BUILDER/controller/CvCreatorController.java
@@ -5,6 +5,7 @@ import dpscvbuilder.com.DPSCV_BUILDER.dto.request.CvCreatorRegisterDto;
 import dpscvbuilder.com.DPSCV_BUILDER.service.CvCreatorService;
 import dpscvbuilder.com.DPSCV_BUILDER.util.constant.UrlConstant;
 import dpscvbuilder.com.DPSCV_BUILDER.util.response.StandardResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = UrlConstant.V1_CV_CREATOR)
 public class CvCreatorController {
 
-    @Autowired
-    private CvCreatorService cvCreatorService;
+    private final CvCreatorService cvCreatorService;
 
     @PostMapping("/register")
     public ResponseEntity<StandardResponse> register(@RequestBody CvCreatorRegisterDto registerDto) {
